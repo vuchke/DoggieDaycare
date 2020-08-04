37596b6ee26d11d0ffa329eb5556801a7361ea88
Vuchke <vuchke@gmail.com>
2020-08-04 20:36:11 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const counter = document.querySelector(".counter");
+const speed = 38;
+const contactSection = document.querySelector(".contact");
+
+const updateCounter = () => {
+  const target = +counter.getAttribute("data-target");
+  const count = +counter.innerText;
+
+  if (count < target) {
+    counter.innerText = count + speed;
+    setTimeout(updateCounter, 20);
+  } else {
+    count.innerText = target;
+  }
+};
+
+contactSection.addEventListener("touchstart mouseover", () => {
+  updateCounter()
+});
