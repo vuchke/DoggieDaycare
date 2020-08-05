d696735e5ddf66e5199b04ec6707537ed41d2d81
Vuchke <vuchke@gmail.com>
2020-08-05 21:41:06 +0200

    new section

--- app.js
+++ app.js
@@ -27,3 +27,29 @@ var observer = new IntersectionObserver(
 
 observer.observe(document.querySelector(".counter"));
 
+/////////////////////////Gallery tabs ////////////////////////
+//displaying opened tabs
+function openTab(tab) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(tab).style.display = "block";  
+}
+
+///// setting active class
+// Get the container element
+var btnContainer = document.querySelector(".tabBtns");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("tabBtn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("tabActive");
+    current[0].className = current[0].className.replace(" tabActive", "");
+    this.className += " tabActive";
+  });
+} 
